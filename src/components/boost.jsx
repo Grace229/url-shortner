fcffd985fdc48371a179742684769e753175e5c5
Grace229 <itiunggrace@gmail.com>
2022-10-28 16:14:43 +0100

    added card component

---
+++ src/components/boost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const boost = () => {
+  return (
+    <div>boost</div>
+  )
+}
+
+export default boost
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -11,18 +11,54 @@ const main = () => {
    </section>
    <section className='advanced-statistics'>
     <div className='first-div'>
-    <h1>
+    <h1 className='advanced-statistics-title'>
     Advanced Statistics
     </h1>
-    <p>
+    <p className='advanced-statistics-text'>
 Track how your links are performing across the web with our 
 advanced statistics dashboard.
     </p>
     </div>
    <div className='cards'>
+  
+<div className='card-wrapper'>
 <div className='card'>
 <div className='image-circle'>
+</div>
+<div className='body-text'>
+<h1 className='card-title'> Brand Recognition</h1>
+<p className='card-text'> Boost your brand recognition with each click. Generic links don’t 
+  mean a thing. Branded links help instil confidence in your content.</p>
+</div>
+</div>
+</div>
+<div className='border'></div>
+<div className='card-wrapper'>
+<div className='card1'>
+<div className='image-circle1'>
+</div>
+<div className='body-text'>
+<h1 className='card-title'>Detailed Records
+</h1>
+<p className='card-text'>
+Gain insights into who is clicking your links. Knowing when and where 
+people engage with your content helps inform better decisions.
+</p>
+</div>
+</div>
+</div>
+<div className='border'></div>
 
+<div className='card-wrapper'>
+<div className='card2'>
+<div className='image-circle2'>
+</div>
+<div className='body-text'>
+<h1 className='card-title'> Fully Customizable</h1>
+<p className='card-text'>
+Improve brand awareness and content discoverability through customizable 
+links, supercharging audience engagement.</p>
+</div>
 </div>
 </div>
    </div>
